f46f1a99582c8f8c4b65f26fe71c41d0bde5cf96
Jakaria Mashud Shahria <jakariamsria@gmail.com>
2021-09-01 13:58:45 +0600

    started

--- linux/flutter/ephemeral/generated_config.cmake
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -1,15 +1,15 @@
 # Generated code do not commit.
 file(TO_CMAKE_PATH "/home/sria/FLUTTER_SDK/flutter" FLUTTER_ROOT)
-file(TO_CMAKE_PATH "/home/sria/AndroidStudioProjects/flutter-app/keeper" PROJECT_DIR)
+file(TO_CMAKE_PATH "/home/sria/AndroidStudioProjects/flutter-app/Haggle Git/keeper" PROJECT_DIR)
 
 # Environment variables to pass to tool_backend.sh
 list(APPEND FLUTTER_TOOL_ENVIRONMENT
   "FLUTTER_ROOT=/home/sria/FLUTTER_SDK/flutter"
-  "PROJECT_DIR=/home/sria/AndroidStudioProjects/flutter-app/keeper"
+  "PROJECT_DIR=/home/sria/AndroidStudioProjects/flutter-app/Haggle Git/keeper"
   "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
   "DART_OBFUSCATION=false"
   "TRACK_WIDGET_CREATION=true"
   "TREE_SHAKE_ICONS=false"
-  "PACKAGE_CONFIG=/home/sria/AndroidStudioProjects/flutter-app/keeper/.dart_tool/package_config.json"
-  "FLUTTER_TARGET=/home/sria/AndroidStudioProjects/flutter-app/keeper/lib/main.dart"
+  "PACKAGE_CONFIG=/home/sria/AndroidStudioProjects/flutter-app/Haggle Git/keeper/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/sria/AndroidStudioProjects/flutter-app/Haggle Git/keeper/lib/main.dart"
 )
